a760f4d117f455a94b130269659c2f9df1f6bdd8
JaoJaoLin <jame31560@gmail.com>
2019-12-13 21:13:26 +0800

    20191213_JaoJao_take out process in Transfer

--- src/com/models/Transfer.java
+++ src/com/models/Transfer.java
@@ -11,6 +11,7 @@ import org.hibernate.Transaction;
 
 import com.controller.GUI;
 import com.models.TransactionDetail;
+
 public class Transfer {
 	public int amount;
 	public int balance;
@@ -18,66 +19,78 @@ public class Transfer {
 	public int traderId;
 	public String otp;
 	public String type;
-	
+
 	public Discounter levelDiscounter; // context has strategy
 	public final int fee = 30;
 	public GUI gui;
 	public TransactionDetail transactionDetail;
-	public Transfer(){
+
+	public Transfer() {
 		gui = new GUI(); // mediator
 	}
+
 	// for testing
-	public Transfer(HttpSession hs){
+	public Transfer(HttpSession hs) {
 		gui = new GUI(hs); // mediator
 	}
-	public void setAmount(int amount){
-	    this.amount=amount;
+
+	public void setAmount(int amount) {
+		this.amount = amount;
 	}
-	public int getAmount(){
-	    return amount;
+
+	public int getAmount() {
+		return amount;
 	}
-	public void setBalance(int balance){
-	    this.balance=balance;
+
+	public void setBalance(int balance) {
+		this.balance = balance;
 	}
-	public int getBalance(){
-	   return balance;
+
+	public int getBalance() {
+		return balance;
 	}
-	public void setTraderId(int traderId){
-	    this.traderId=traderId;
+
+	public void setTraderId(int traderId) {
+		this.traderId = traderId;
 	}
-	public int getTraderId(){
-	   return traderId;
+
+	public int getTraderId() {
+		return traderId;
 	}
-	public void setUserId(int userId){
-	    this.userId=userId;
+
+	public void setUserId(int userId) {
+		this.userId = userId;
 	}
-	public int getUserId(){
-	   return userId;
+
+	public int getUserId() {
+		return userId;
 	}
-	public void setType(String type){
-	    this.type=type;
+
+	public void setType(String type) {
+		this.type = type;
 	}
-	public String getType(){
-	   return type;
+
+	public String getType() {
+		return type;
 	}
-	public void setDiscounter(Discounter lev){
+
+	public void setDiscounter(Discounter lev) {
 		this.levelDiscounter = lev;
 	}
-	public double getDiscount(){
+
+	public double getDiscount() {
 		return levelDiscounter.getDiscount(amount);
 	}
-	public void setOtp(String otp){
-	    this.otp=otp;
+
+	public void setOtp(String otp) {
+		this.otp = otp;
 	}
-	public String getOtp(){
-	   return otp;
+
+	public String getOtp() {
+		return otp;
 	}
-	
-	public String process() {
-		return "success";
-	}
+
 	public String setTransactionDetail(int walletId) {
 		return gui.setTransferDetail(this, walletId);
 	}
 }
-
