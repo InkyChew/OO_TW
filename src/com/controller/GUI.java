27b7964cd8a1f35553d9807375c33c7844721772
JaoJao <jame31560@gmail.com>
2019-12-16 22:53:09 +0800

    20191216_JaoJao_update err msg
    ~under or equal than~
    change to
    ~equal or under than~

--- src/com/controller/GUI.java
+++ src/com/controller/GUI.java
@@ -2,7 +2,6 @@ package com.controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-
 import org.apache.struts2.ServletActionContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -41,7 +40,7 @@ import com.models.Deposit;
 import com.models.Admin;
 
 // Facade+Mediator
-public class GUI { //GUI=V+C
+public class GUI { // GUI=V+C
 	private User user = new User();
 	private Admin admin = new Admin();
 	private Transfer transfer;
@@ -51,119 +50,137 @@ public class GUI { //GUI=V+C
 	private List<User> userList = new ArrayList<User>();
 	private Mail mail;
 	private Auth auth;
-	
+
 	private RegisterOriginator registerOriginator;
 	private RegisterCareTaker registerCareTaker;
 	private OrCtStored orCtStored;
-	
+
 	// DB
 	Session session = null;
 	Transaction tx = null;
-	
+
 	public GUI() {
-		this.mail = Mail.getInstance(); //Singleton
+		this.mail = Mail.getInstance(); // Singleton
 		this.auth = new Auth();
 	}
+
 	// for testing
 	public GUI(HttpSession hs) {
-		this.mail = Mail.getInstance(); //Singleton
+		this.mail = Mail.getInstance(); // Singleton
 		this.auth = new Auth(hs); // testing
 	}
-	public void setUser(User user){
-	    this.user=user;
+
+	public void setUser(User user) {
+		this.user = user;
 	}
-	public User getUser(){
-	    return user;
+
+	public User getUser() {
+		return user;
 	}
-	public void setTransfer(Transfer transfer){
-	    this.transfer=transfer;
+
+	public void setTransfer(Transfer transfer) {
+		this.transfer = transfer;
 	}
-	public Transfer getTransfer(){
-	    return transfer;
+
+	public Transfer getTransfer() {
+		return transfer;
 	}
+
 	public void setAbTransfer(AbTransfer abT) {
 		abTransfer = abT;
 	}
+
 	public AbTransfer getAbTransfer() {
 		return abTransfer;
 	}
-	public void setTransactionDetails(List<TransactionDetail> transactionDetails){
-	    this.transactionDetails=transactionDetails;
+
+	public void setTransactionDetails(List<TransactionDetail> transactionDetails) {
+		this.transactionDetails = transactionDetails;
 	}
-	public List<TransactionDetail> getTransactionDetails(){
-	    return transactionDetails;
+
+	public List<TransactionDetail> getTransactionDetails() {
+		return transactionDetails;
 	}
-	public void setUserList(List<User> userList){
-	    this.userList=userList;
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
 	}
-	public List<User> getUserList(){
-	    return userList;
+
+	public List<User> getUserList() {
+		return userList;
 	}
+
 	public String toPlatform() {
 		String output = "error";
-		if(auth.checkSession()){
+		if (auth.checkSession()) {
 			output = "success";
-			if(auth.isAdmin()) {
+			if (auth.isAdmin()) {
 				output = "administrator";
 			}
 		}
 		return output;
-	}	
+	}
+
 	public String toDeposit() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			output = "success";
 		}
 		return output;
 	}
+
 	public String ToScan() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			output = "success";
 		}
 		return output;
 	}
+
 	public String toReceivement() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			user = auth.getCurrentUser();
 			output = "success";
 		}
 		return output;
 	}
+
 	public String toInformation() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			user = auth.getCurrentUser();
 			output = "success";
 		}
 		return output;
 	}
+
 	public String toServiceDetails() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			user = auth.getCurrentUser();
 			output = "success";
 		}
 		return output;
 	}
-	
+
 	public String deposit() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			int userId = auth.getUserId();
-			int amount= transfer.amount;
+			int amount = transfer.amount;
 			abTransfer = new AbTransfer(userId, userId, amount, new Deposit());
 			output = abTransfer.process();
 		}
 		return output;
 	}
+
 	public String login() {
 		String output = "error";
-		if(auth.createSession()) {
-			if(auth.isAdmin()) {
+		if (auth.createSession()) {
+			if (auth.isAdmin()) {
 				output = "administrator";
-			}else {
+			} else {
 				output = "success";
 			}
 		} else {
@@ -174,19 +191,19 @@ public class GUI { //GUI=V+C
 
 	public String showPayOTPView() {
 		String output = "error";
-		if(auth.checkSession()) {
+		if (auth.checkSession()) {
 			// sendOTP
 			String OTP = "";
-			for(int i = 0; i < 8; i++){
-		      int random = (int)((Math.random() * 3) + 1);
-		      if(random == 1){
-		    	OTP += (char)(int)((Math.random()*10)+48);
-		      }else if(random == 2){
-		        OTP += (char)(int)(((Math.random()*26) + 65));
-		      }else{
-		        OTP += (char)(int)((Math.random()*26) + 97);
-		      }
-		    }
+			for (int i = 0; i < 8; i++) {
+				int random = (int) ((Math.random() * 3) + 1);
+				if (random == 1) {
+					OTP += (char) (int) ((Math.random() * 10) + 48);
+				} else if (random == 2) {
+					OTP += (char) (int) (((Math.random() * 26) + 65));
+				} else {
+					OTP += (char) (int) ((Math.random() * 26) + 97);
+				}
+			}
 			user = auth.getCurrentUser();
 			String address = user.getUserInfo().getEmail();
 			mail.sendOTP(address, OTP);
@@ -197,13 +214,14 @@ public class GUI { //GUI=V+C
 		}
 		return output;
 	}
+
 	public String payment() {
 		String output = "error";
-		if(auth.checkSession()) {
-			int traderId=transfer.traderId;
-			int amount= transfer.amount;
+		if (auth.checkSession()) {
+			int traderId = transfer.traderId;
+			int amount = transfer.amount;
 			String otp = transfer.otp;
-			if(auth.checkOTP(otp)) {
+			if (auth.checkOTP(otp)) {
 				abTransfer = new AbTransfer(auth.getUserId(), traderId, amount, new Payment());
 				output = abTransfer.process();
 			} else {
@@ -214,49 +232,54 @@ public class GUI { //GUI=V+C
 		}
 		return output;
 	}
+
 	public void receivement(int userId, int traderId, int amount) {
 		abTransfer = new AbTransfer(userId, traderId, amount, new Receivement());
 		abTransfer.process();
 	}
+
 	// for testing
 	public void receivement(int userId, int traderId, int amount, HttpSession hs) {
 		abTransfer = new AbTransfer(userId, traderId, amount, new Receivement(hs));
 		abTransfer.process();
 	}
+
 	public String checkTransactionHistory() {
 		String output = "error";
 		session = HibernateUtil.getSessionFactory().openSession();
-		if(auth.checkSession()) {
-			try{
-		         tx = session.beginTransaction();
-		   		 int userId = auth.getUserId();
-		   		 List data = session.createCriteria(User.class).add(Restrictions.eq("userId", userId)).list();
-		   		 int walletId = ((User) data.get(0)).wallet.walletId;
-		         data = session.createCriteria(TransactionDetail.class).add(Restrictions.eq("walletId", walletId)).list();
-		         
-		         for (int i = 0; i < data.size(); i++) {
-		        	 transactionDetail = (TransactionDetail) data.get(i);
-		        	 transactionDetails.add(transactionDetail);
-		         }
-		 		 data.clear();
-		         tx.commit();
-		         output="success";
-		      }catch (HibernateException e) {
-		         if (tx!=null) tx.rollback();
-		         e.printStackTrace(); 
-		      }finally {
-		         session.close(); 
-		      }
+		if (auth.checkSession()) {
+			try {
+				tx = session.beginTransaction();
+				int userId = auth.getUserId();
+				List data = session.createCriteria(User.class).add(Restrictions.eq("userId", userId)).list();
+				int walletId = ((User) data.get(0)).wallet.walletId;
+				data = session.createCriteria(TransactionDetail.class).add(Restrictions.eq("walletId", walletId))
+						.list();
+
+				for (int i = 0; i < data.size(); i++) {
+					transactionDetail = (TransactionDetail) data.get(i);
+					transactionDetails.add(transactionDetail);
+				}
+				data.clear();
+				tx.commit();
+				output = "success";
+			} catch (HibernateException e) {
+				if (tx != null)
+					tx.rollback();
+				e.printStackTrace();
+			} finally {
+				session.close();
+			}
 		}
 		return output;
 	}
-	
+
 	public String logout() {
 		auth.removeSession();
 		String output = "success";
 		return output; // login page
 	}
-	
+
 	public String setTransferDetail(Transfer transfer, int walletId) {
 		DateFormat dfcurrentTime = new SimpleDateFormat("yyyyMMddHHmmss");
 		String date = dfcurrentTime.format(new java.util.Date());
@@ -269,7 +292,7 @@ public class GUI { //GUI=V+C
 		transactionDetail.setTraderId(transfer.traderId);
 		return transactionDetail.updateTransactionDetail();
 	}
-	
+
 	public User getAuthUser(int userId) {
 		return auth.getUser(userId);
 	}
@@ -278,24 +301,24 @@ public class GUI { //GUI=V+C
 		String output = "error";
 		if (auth.checkSession()) {
 			if (auth.isAdmin()) {
-		         userList = admin.getAllUser();
-		         output = "administrator";
+				userList = admin.getAllUser();
+				output = "administrator";
 			}
 		}
 		return output;
 	}
-	
+
 	private void setOrCt(String key) {
 		this.registerOriginator = this.orCtStored.getRegisterOriginator(key);
 		this.registerCareTaker = this.orCtStored.getRegisterCareTaker(key);
 	}
-	
+
 	public String toRegisterContract() {
 		this.orCtStored = OrCtStored.getInstance();
 		String result = "error";
 		HttpServletRequest httpRequest = ServletActionContext.getRequest();
 		String method = (String) httpRequest.getMethod();
-		String type = (String)httpRequest.getParameter("type");
+		String type = (String) httpRequest.getParameter("type");
 		String key = this.auth.getOrCTID();
 		if (method.equals("POST")) {
 			if (type.equals("back")) {
@@ -306,7 +329,7 @@ public class GUI { //GUI=V+C
 				result = "back";
 			} else {
 				this.setOrCt(key);
-				String contract = (String)httpRequest.getParameter("contract");
+				String contract = (String) httpRequest.getParameter("contract");
 				if (contract == null) {
 					httpRequest.setAttribute("msg", "please accept the contract.");
 					return "wrong";
@@ -320,7 +343,7 @@ public class GUI { //GUI=V+C
 		} else { // GET
 			if (key == null) {
 				key = this.auth.CreatOrCTID();
-				while(!this.orCtStored.OrCTIDIsEmpty(key)) {
+				while (!this.orCtStored.OrCTIDIsEmpty(key)) {
 					key = this.auth.CreatOrCTID();
 				}
 				this.registerOriginator = this.orCtStored.addRegisterOriginator(key);
@@ -338,13 +361,13 @@ public class GUI { //GUI=V+C
 		}
 		return result;
 	}
-	
+
 	public String toRegisterInfo() throws IllegalAccessException {
 		this.orCtStored = OrCtStored.getInstance();
 		String result = "error";
 		HttpServletRequest httpRequest = ServletActionContext.getRequest();
 		String method = (String) httpRequest.getMethod();
-		String type = (String)httpRequest.getParameter("type");
+		String type = (String) httpRequest.getParameter("type");
 		String key = this.auth.getOrCTID();
 		if (method.equals("POST")) {
 			this.setOrCt(key);
@@ -362,11 +385,12 @@ public class GUI { //GUI=V+C
 				this.registerOriginator.setName(name);
 				this.registerOriginator.setTelephone(telephone);
 				this.registerOriginator.setAddress(address);
-				if(email.equals("") || username.equals("") || name.equals("") || telephone.equals("") || address.equals("")) {
+				if (email.equals("") || username.equals("") || name.equals("") || telephone.equals("")
+						|| address.equals("")) {
 					httpRequest.setAttribute("msg", "All fields are required.");
 					return "wrong";
 				} else if (username.length() > 10) {
-					httpRequest.setAttribute("msg", "Your username must be under or equal than 10 characters long.");
+					httpRequest.setAttribute("msg", "Your username must be equal or under than 10 characters long.");
 					return "wrong";
 				} else if (auth.userExist(username)) {
 					httpRequest.setAttribute("msg", "This username has been used.");
@@ -374,16 +398,16 @@ public class GUI { //GUI=V+C
 				}
 				// sendOTP
 				String OTP = "";
-				for(int i = 0; i < 8; i++){
-			      int random = (int)((Math.random() * 3) + 1);
-			      if(random == 1){
-			    	OTP += (char)(int)((Math.random()*10)+48);
-			      }else if(random == 2){
-			        OTP += (char)(int)(((Math.random()*26) + 65));
-			      }else{
-			        OTP += (char)(int)((Math.random()*26) + 97);
-			      }
-			    }
+				for (int i = 0; i < 8; i++) {
+					int random = (int) ((Math.random() * 3) + 1);
+					if (random == 1) {
+						OTP += (char) (int) ((Math.random() * 10) + 48);
+					} else if (random == 2) {
+						OTP += (char) (int) (((Math.random() * 26) + 65));
+					} else {
+						OTP += (char) (int) ((Math.random() * 26) + 97);
+					}
+				}
 				mail.sendConfirmMail(email, OTP);
 				final LocalDateTime expire = LocalDateTime.now(Clock.system(ZoneId.of("+8"))).plusMinutes(10);
 				auth.createOTP(OTP, expire);
@@ -412,13 +436,13 @@ public class GUI { //GUI=V+C
 		}
 		return result;
 	}
-	
+
 	public String toRegisterPassword() throws IllegalAccessException {
 		this.orCtStored = OrCtStored.getInstance();
 		String result = "error";
 		HttpServletRequest httpRequest = ServletActionContext.getRequest();
 		String method = (String) httpRequest.getMethod();
-		String type = (String)httpRequest.getParameter("type");
+		String type = (String) httpRequest.getParameter("type");
 		String key = this.auth.getOrCTID();
 		if (method.equals("POST")) {
 			this.setOrCt(key);
@@ -429,9 +453,11 @@ public class GUI { //GUI=V+C
 				String inputOTP = (String) httpRequest.getParameter("OTP");
 				String password = (String) httpRequest.getParameter("password");
 				String confirmPassword = (String) httpRequest.getParameter("confirmPassword");
-				if(this.auth.checkOTP(inputOTP)) {
-					if (password.equals("") || confirmPassword.equals("") || password.length() > 10 || confirmPassword.length() > 10) {
-						httpRequest.setAttribute("msg", "Password & confirm password is required and must be under or equal than 10 characters long.");
+				if (this.auth.checkOTP(inputOTP)) {
+					if (password.equals("") || confirmPassword.equals("") || password.length() > 10
+							|| confirmPassword.length() > 10) {
+						httpRequest.setAttribute("msg",
+								"Password & confirm password is required and must be equal or under than 10 characters long.");
 						return "wrong";
 					} else if (!password.equals(confirmPassword)) {
 						httpRequest.setAttribute("msg", "Password & confirm password must be the same.");
@@ -469,7 +495,7 @@ public class GUI { //GUI=V+C
 		}
 		return result;
 	}
-	
+
 	public String toRegisterDone() {
 		return "success";
 	}
